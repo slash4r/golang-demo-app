c014b22d0827508493a7ecfdb10d05a4a91972b8
Denys Mykhailov <necuknpocto@gmail.com>
2024-12-23 01:04:04 +0200

    extra-task

---
+++ terraform-aws/db_schema.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE dbgolang;
+\c dbgolang;
+CREATE TABLE IF NOT EXISTS videos (
+  id VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL
+);
---
+++ terraform-aws/variables.tf
@@ -0,0 +1,19 @@
+variable "db_username" {
+  description = "The database username"
+  default = "postgres"
+}
+
+variable "db_password" {
+  description = "The database password"
+  default = "password"
+}
+
+variable "db_name" {
+  description = "The database name"
+  default = "dbgolang"
+}
+
+variable "db_endpoint" {
+  description = "The database endpoint"
+  default = "localhost"
+}
